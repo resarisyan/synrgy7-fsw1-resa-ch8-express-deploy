bad6ab29c9c9a8e8de54831b3d7bcaf5ba654992
resarisyan <resarisyan77@gmail.com>
2024-06-26 20:40:27 +0700

    fix: change redis config

--- src/repositories/base/RedisRepository.ts
+++ src/repositories/base/RedisRepository.ts
@@ -12,7 +12,9 @@ export class RedisRepository<T> {
   private async initializeRedisClient(): Promise<void> {
     try {
       this.redisClient = createClient({
-        url: 'redis://localhost:6379',
+        url: 'redis-13628.c10.us-east-1-2.ec2.redns.redis-cloud.com:13628',
+        password: 'ojg6xrNo8KzQAhR0YLF7z4qscM8nGFnW',
+        username: 'default',
         socket: {
           connectTimeout: 1000
         }
